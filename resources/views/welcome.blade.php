498c44d84637fa3f108aa8cb7695cda574ab77f2
BL <115746391+Mr-BLN@users.noreply.github.com>
2025-07-03 14:37:57 +0200

    add new components for homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-    Hello World
+    <section>
+        <x-section-heading>Featured Jobs</x-section-heading>
+        <div class="grid lg:grid-cols-3 gap-8 mt-6">
+            <x-job-card/>
+            <x-job-card/>
+            <x-job-card/>
+        </div>
+    </section>
+
 </x-layout>
